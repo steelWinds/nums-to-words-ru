d0ba37c8e8db4f479fffb920f81667a2c272e275
ninja <kirillsurov0@gmail.com>
2022-04-02 23:52:29 +0900

    fix(class): remove console.log call from Converter class

--- src/modules/classes/Converter.ts
+++ src/modules/classes/Converter.ts
@@ -119,8 +119,6 @@ class Converter implements IConverter {
         const tens = num % 10
         const hundredts = num % 100
 
-        console.log(num, tens, hundredts)
-
         if (tens === 1 && (hundredts < 10 || hundredts > 19)) {
             word = group[0]
         } else if (tens > 1 && tens < 5 && hundredts > 19) {
